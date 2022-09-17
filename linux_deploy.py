6d7a7e6d70195920c76aa29dbb917a30269aae24
hh505030475 <505030475@qq.com>
2022-09-17 15:39:53 +0800

    k

--- linux_deploy.py
+++ linux_deploy.py
@@ -14,8 +14,8 @@ except:
 
 print('download uhmap file manifest | 下载manifest目录文件')
 manifest_url = "https://ageasga-my.sharepoint.com/:u:/g/personal/fuqingxu_yiteam_tech/EVmCQMSUWV5MgREWaxiz_GoBalBRV3DWBU3ToSJ5OTQaLQ?e=I8yjl9"
-download(manifest_url, filename="./Temp/manifest.jsonc")
-with open("./Temp/manifest.jsonc", "r") as f:
+file = download(manifest_url, filename="./Temp/")
+with open("./Temp/uhmap_manifest.jsonc", "r") as f:
     manifest = commentjson.load(f)
 
 # download unreal engine visual file | 下载虚幻引擎文件
@@ -24,8 +24,8 @@ try:
 except:
     pass
 
-if not os.path.exists("./UnrealEngine/%s"%VERSION):
+if not os.path.exists("./UnrealEngine/%s.zip"%VERSION):
     uhmap_url = manifest[VERSION]
-    download(uhmap_url, filename="./UnrealEngine/%s"%VERSION, unzip=True, unzip_path='./UnrealEngine')
+    file = download(uhmap_url, filename="./UnrealEngine/", unzip=True, unzip_path='./UnrealEngine')
 else:
     print('Unreal engine binary exists, skip download.. | 虚幻引擎文件已存在, 跳过下载, 若重试请删除UnrealEngine文件夹')
